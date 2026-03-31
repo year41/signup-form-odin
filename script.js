54751cb2fa0ad0c79d5594f31aa53c5055f4d3eb
dado <162951673+year41@users.noreply.github.com>
2026-03-31 18:34:25 +1100

    Add js function to the button "theme-color"

---
+++ script.js
@@ -0,0 +1,18 @@
+function systemTheme() {
+    const root = document.documentElement;
+    const mode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    const newTheme =  mode ? "dark" : "light";
+    root.className = newTheme;
+}
+
+function setTheme() {
+    const root = document.documentElement;
+    const newTheme = root.className === "light" ? "dark" : "light";
+    root.className = newTheme;
+
+    // document.querySelector(".theme-name").textContent = newTheme;
+}
+
+systemTheme();
+
+document.querySelector(".theme-color").addEventListener('click', setTheme);
