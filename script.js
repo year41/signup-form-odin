1ed76661942c351418fa93010f821b6c74b180e1
dado <162951673+year41@users.noreply.github.com>
2026-04-01 18:07:13 +1100

    Add color to dark mode, remove js test lines

--- script.js
+++ script.js
@@ -3,15 +3,12 @@ function systemTheme() {
     const mode = window.matchMedia("(prefers-color-scheme: dark)").matches;
     const newTheme =  mode ? "dark" : "light";
     root.className = newTheme;
-    root.className = "light"; //editing light-mode for now choosing color and all
 }
 
 function setTheme() {
     const root = document.documentElement;
     const newTheme = root.className === "light" ? "dark" : "light";
     root.className = newTheme;
-
-    // document.querySelector(".theme-name").textContent = newTheme;
 }
 
 systemTheme();
